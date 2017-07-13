d077067e36ffc9942067e7e523e9bed3c61827ac
BlackBeardEC <rich.stadnick@gmail.com>
2017-07-13 15:25:44 -0400

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,22 @@
 var socket = io();
 
+function scrollToBottom() {
+  // selectors
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child');
+
+  // heights
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+    messages.scrollTop(scrollHeight);
+  }
+}
+
 socket.on('connect', function(){
   console.log('Connected to server.');
 
@@ -21,7 +38,7 @@ socket.on('newMessage', function(msg){
   });
 
   jQuery('#messages').append(html);
-
+  scrollToBottom();
 });
 
 socket.on('newLocationMessage', function (msg){
@@ -34,6 +51,7 @@ socket.on('newLocationMessage', function (msg){
   });
 
   jQuery('#messages').append(html);
+  scrollToBottom();
 
 });
 
